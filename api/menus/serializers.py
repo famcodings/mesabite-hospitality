fbd39290ee3c40367e031cf6a1a360f540f62ea7
famcodings <numanfarukh.work@gmail.com>
2023-11-06 18:49:31 +0500

    override list method of CategoriesViewset to group searched categories based on folder

--- api/menus/serializers.py
+++ api/menus/serializers.py
@@ -3,15 +3,23 @@ from rest_framework import serializers
 from menus.models import Folder, Category
 
 
+class CategorySerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Category
+        fields = ('__all__')
+
+
 class FolderSerializer(serializers.ModelSerializer):
     
+    categories = CategorySerializer(read_only=True, many=True)
     class Meta:
         model = Folder
         fields = ('__all__')
 
 
-class CategorySerializer(serializers.ModelSerializer):
+class FolderDetailSerializer(serializers.ModelSerializer):
     
     class Meta:
-        model = Category
+        model = Folder
         fields = ('__all__')
